7891074ed865cfe6fb0eaa94900a82411f6eef04
Ankit Thakur <ankitthakur3110@gmail.com>
2026-04-06 12:10:32 +0530

    update next-config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   // Change this to your GitHub repo name if deploying to /<repo-name>/
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
 }
 
 module.exports = nextConfig
